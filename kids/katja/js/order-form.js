36f38cabedecddbb2516fd3d88b59838650b0568
HughP <40230+HughP@users.noreply.github.com>
2026-08-07 20:22:08 -0400

    Update Google Apps Script URL in order-form.js

--- kids/katja/js/order-form.js
+++ kids/katja/js/order-form.js
@@ -7,7 +7,7 @@ let inventoryData = {};
 let cart = [];
 
 // Google Apps Script Web App URL (you'll need to create this)
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzCgZyuBcoPprWBsqwcc0gu0vNqrcuLlk3I-YP-Kv3WOzhMs7Jdfcby-uLYRdWE0eNk/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw6TM1mk7a3pWWe6PGjdRvPWpSTHbcEiIaBXxHf06XBpUTk58AipddSzyCf0WzH6YtH/exec';
 
 // Pricing for jar sizes
 const PRICING = {
